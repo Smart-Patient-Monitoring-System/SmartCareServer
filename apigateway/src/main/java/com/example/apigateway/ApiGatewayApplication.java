82c8130395f8edb1a40b43f85576f1d9115fa5f8
Kavindi487 <kavindividusari@gmail.com>
2026-02-23 22:54:46 +0530

    updated cores

--- apigateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ apigateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -2,10 +2,29 @@ package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    //  Global CORS configuration
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://frontend.mangobush-8de88b36.southeastasia.azurecontainerapps.io"); // your frontend
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*"); // GET, POST, PUT, DELETE, OPTIONS
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
